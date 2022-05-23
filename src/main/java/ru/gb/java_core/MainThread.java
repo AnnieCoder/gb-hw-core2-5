17d2513919b775db66e61f79cc9293b004bee2ec
annartemyeva <artemeva.a.o@gmail.com>
2022-05-23 21:59:10 +0300

    Initial commit

---
+++ src/main/java/ru/gb/java_core/MainThread.java
@@ -0,0 +1,54 @@
+package ru.gb.java_core;
+
+import java.util.Arrays;
+
+public class MainThread {
+
+    static final int size = 10000000;
+    static final int h = size / 2;
+
+    public static void main(String[] args) {
+
+        float[] arr = new float[size];
+        float[] arr1 = new float[h];
+        float[] arr2 = new float[h];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        long a = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, arr1, 0, h);
+        System.arraycopy(arr, h, arr2, 0, h);
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                arr1[i] = (float) (arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.arraycopy(arr1, 0, arr, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+
+        System.out.println(System.currentTimeMillis() - a);
+
+    }
+
+}
